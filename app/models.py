f8ea1776a6c8fdd5a63ced1e66229b902588a43d
Ivan Kozhin <kothin.ivan@gmail.com>
2023-05-19 19:18:06 +0500

    Create models.py
    models.py defines the data model for the "questions" table

---
+++ app/models.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.declarative import declarative_base
+
+from datetime import datetime
+
+from .database import Base
+
+
+# Model of request to the table "questions" in database
+class QuizIn(BaseModel):
+    questions_num: int
+
+
+class Question(Base):
+    """
+    Defines the questions model
+    """
+
+    __tablename__ = "questions"
+
+    id = Column(Integer, primary_key=True)
+    question_text = Column(String(200), unique=True)
+    answer_text = Column(String(200))
+    creation_date = Column(DateTime, default=datetime.utcnow)
+
+    def __init__(self, id: int, question_text: str, answer_text: str, creation_date: datetime):
+        self.id = id
+        self.question_text = question_text
+        self.answer_text = answer_text
+        self.creation_date = creation_date
+
+    def __repr__(self) -> str:
+        return f"<Question number {self.id}: {self.question_text}>"
