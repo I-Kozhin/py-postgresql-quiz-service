0226caab55bb5e7ff4059834db5a6cd2ba672a0d
Ivan Kozhin <kothin.ivan@gmail.com>
2023-05-19 20:02:48 +0500

    Created crud.py | updated models.py
    crud.py provides a basic operation for working with the objects of the Question model in the database, such as getting the requested number of quiz questions from an external API

---
+++ app/crud.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from sqlalchemy.orm import Session  # type: ignore
+
+from .models import Question
+
+import requests
+
+from fastapi import HTTPException
+
+def get_random_questions(num_questions: int) -> List[Question] | Question:
+    url = f"https://jservice.io/api/random?count={num_questions}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Failed to fetch questions from API")
--- app/models.py
+++ app/models.py
@@ -1,10 +1,6 @@
-from pydantic import BaseModel
-
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.ext.declarative import declarative_base
-
 from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime
 
 from .database import Base
 
@@ -26,6 +22,3 @@ class Question(Base):
         self.question_text = question_text
         self.answer_text = answer_text
         self.creation_date = creation_date
-
-    def __repr__(self) -> str:
-        return f"<Question number {self.id}: {self.question_text}>"
