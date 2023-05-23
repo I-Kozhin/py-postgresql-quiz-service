7a71e5e98a35699939001ada270ca7e11e5ac958
Ivan Kozhin <kothin.ivan@gmail.com>
2023-05-23 16:39:13 +0500

    Refactor v2
    Separate External API Call from Business Logic:
    Create a new class QuestionApiService that is responsible for fetching questions from the external API.

--- service/question_service.py
+++ service/question_service.py
@@ -17,15 +17,19 @@ class QuestionService:
 
     def __init__(self):
         self.question_repository = QuestionRepository()
+        self.question_api_service = QuestionApiService()
 
     def create_unique_questions(self, question_count: int) -> None:
-        unique_questions = self.__get_unique_questions_from_api(question_count)
+        unique_questions = self.question_api_service.get_unique_questions_from_api(question_count)
         self.question_repository.create_questions(unique_questions)
 
     def get_last_question(self):
         return self.question_repository.get_last_question_nullable()
 
-    def __get_unique_questions_from_api(self, question_count: int) -> List[QuestionDto]:
+
+class QuestionApiService:
+    @staticmethod
+    def get_unique_questions_from_api(question_count: int) -> List[QuestionDto]:
         unique_questions_from_api = []
         while len(unique_questions_from_api) < question_count:
             url = f"https://jservice.io/api/random?count={1}"
@@ -33,11 +37,7 @@ class QuestionService:
             if response.status_code == 200:
                 question_data = response.json()
                 question = QuestionDto.parse_obj(question_data[0])
-                #  есть ли вопрос в бд
-                if self.question_repository.is_question_exist(question.question):
-                    continue
-                else:
-                    unique_questions_from_api.append(question)
+                unique_questions_from_api.append(question)
             else:
                 raise QuestionServiceError(f"Failed to fetch questions from API: {response.status_code}")
 
